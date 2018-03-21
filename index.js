911e8f9840518b5a5bf36284beab6d24e4ff5a3f
Sara Tibbetts <sxt4866@rit.edu>
2018-03-21 01:51:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,95 @@
+let store = {
+  deliveries: [], 
+  meals: [], 
+  employers: [], 
+  customers: []
+};
+
+let deliveryId = 0; 
+
+class Delivery {
+  constructor(meal = {}, customer = {}){
+    this.mealId = meal.id; 
+    this.customerId = customer.id; 
+    this.id = deliveryId++; 
+    store.deliveries.push(this); 
+  }
+  
+  customer(){
+    return store.customers.find(customer => {
+      return customer.id === this.customerId; 
+    })
+  }
+  
+  meal(){
+    return store.meals.find(meal => {
+      return meal.id === this.mealId; 
+    })
+  }
+}
+
+let mealId = 0; 
+class Meal {
+  constructor(title, price){
+    this.title = title; 
+    this.price = price; 
+    this.id = mealId++; 
+    store.meals.push(this); 
+  }
+  
+  deliveries(){
+    return store.deliveries.filter(delivery => {
+      return delivery.mealId === this.id; 
+    });
+  }
+  
+  customers(){
+    return this.deliveries().map(delivery => {
+      return delivery.customer(); 
+    })
+  }
+  
+  static byPrice(){
+    return store.meals.sort((meal1, meal2) => 
+      {
+        return meal1.price < meal2.price; 
+      }); 
+  }
+}
+
+let employerId = 0; 
+class Employer {
+  constructor(name){
+    this.name = name; 
+    this.id = employerId++; 
+    store.employers.push(this); 
+  }
+}
+
+let customerId = 0; 
+class Customer{
+  constructor(name){
+    this.name = name; 
+    this.id = customerId++; 
+    store.customers.push(this); 
+  }
+  
+  deliveries(){
+    return store.deliveries.filter(delivery => {
+      return delivery.customerId === this.id; 
+    });
+  }
+  
+  meals(){
+    return this.deliveries().map( delivery => {
+      return delivery.meal(); 
+    })
+  }
+  
+  totalSpent(){
+    return this.meals().reduce(function(meal, sum){
+      debugger; 
+      return sum + meal.price; 
+    }, 0); 
+  }
+}
